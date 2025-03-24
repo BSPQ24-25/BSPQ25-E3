58cc4173e2a11299b1aac02daa296cbb7b534004
erkudenkamiruaga <erkuden.camiruaga@opendeusto.es>
2025-03-24 13:32:24 +0100

    Repository made

---
+++ src/main/java/com/student_loan/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.student_loan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.student_loan.model.Loan;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+}
---
+++ src/main/java/com/student_loan/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.student_loan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.student_loan.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
