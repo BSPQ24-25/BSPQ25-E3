8d3f167a611e77967d77f957d3d8e468099255bf
Luis-Rdguez <rodriguez.luis@opendeusto.es>
2025-03-24 12:38:12 +0100

    Maven Project Launch

---
+++ src/test/java/com/student_loan/StudentLoanManagement/StudentLoanManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student_loan.StudentLoanManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoanManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
