e5545d6dcd5ab7b4ab3bd3280c07a557497c5f4f
erkudenkamiruaga <erkuden.camiruaga@opendeusto.es>
2025-04-11 17:42:40 +0200

    Creation of test files only with imports (no content inside yest)

---
+++ backend/src/test/java/com/student_loan/integration/IntegrationTest.java
@@ -0,0 +1,19 @@
+package com.student_loan.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import com.student_loan.model.Item;
+import com.student_loan.model.Loan;
+import com.student_loan.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+
+public class IntegrationTest {
+    
+}
