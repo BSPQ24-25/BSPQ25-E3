7d630776d6141a383a63fc2fa852a4df1052aeba
sabinluja <sabin.luja@opendeusto.es>
2025-04-26 12:49:54 +0200

    performance tests working

--- backend/src/test/java/com/student_loan/performance/ThroughputTest.java
+++ backend/src/test/java/com/student_loan/performance/ThroughputTest.java
@@ -14,7 +14,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("performance")
 @SpringBootTest
 @ExtendWith(JUnitPerfInterceptor.class)
 public class ThroughputTest {
